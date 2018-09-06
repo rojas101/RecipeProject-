b59521929515bfe52c2eddd079f94623f6d7645c
Ryan Rojas <theryanrojas@gmail.com>
2018-09-06 20:26:49 +0000

    Completed individual recipe page, updated homepage links

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,11 @@
 class RecipesController < ApplicationController
     
     def index 
-        @recipes = Recipe.all
+        @recipe = Recipe.all
+    end 
+    
+    def show 
+        @recipe = Recipe.find(params[:id])
     end 
     
 end 
